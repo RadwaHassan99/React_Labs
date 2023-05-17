4c43a9c515d427cf1bdf3e112688dd941c1c17c6
Radwa99 <radwa.prof.9@gmail.com>
2023-05-17 10:17:12 +0300

    edit lab2

--- lab2/src/Components/Routing/RegistrationForm.js
+++ lab2/src/Components/Routing/RegistrationForm.js
@@ -1,62 +1,77 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const RegistrationForm = ({ handleRegistration }) => {
-  const [id, setId] = useState('');
-  const [name, setName] = useState('');
-  const [age, setAge] = useState('');
+class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      name: '',
+      age: ''
+    };
+  }
 
-  const handleSubmit = (e) => {
+  handleSubmit = (e) => {
     e.preventDefault();
+    const { id, name, age } = this.state;
     const newUser = { id, name, age };
-    handleRegistration(newUser);
-    setId('');
-    setName('');
-    setAge('');
+    this.props.handleRegistration(newUser);
+    this.setState({
+      id: '',
+      name: '',
+      age: ''
+    });
   };
 
-  return (
-    <div className="card register">
-      <div className="card-header">Registration Form</div>
-      <div className="card-body">
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            
-            <input
-              type="text"
-              className="form-control"
-              id="id"
-              placeholder="Enter ID"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-           
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              placeholder="Enter Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-       
-            <input
-              type="number"
-              className="form-control"
-              id="age"
-              placeholder="Enter Age"
-              value={age}
-              onChange={(e) => setAge(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  render() {
+    const { id, name, age } = this.state;
+
+    return (
+      <div className="card register">
+        <div className="card-header">Registration Form</div>
+        <div className="card-body">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                id="id"
+                placeholder="Enter ID"
+                value={id}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Enter Name"
+                value={name}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="number"
+                className="form-control"
+                id="age"
+                placeholder="Enter Age"
+                value={age}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default RegistrationForm;
